7e9d6a5bd961623655bf98d00e654c2f9615ce32
Jéssica Hatsumi Isibasi <jessicaisibasi@gmail.com>
2023-12-20 12:14:01 -0300

    modal quase pronto

--- script.js
+++ script.js
@@ -15,3 +15,25 @@ function updateCarousel() {
     const slideWidth = document.querySelector('.carousel-image').clientWidth;
     imageContainer.style.transform = `translateX(${-currentIndex * slideWidth}px)`;
 }
+
+function abrirModal(botao) {
+    var modal = document.getElementById("modalProduto");
+    document.getElementById("modal-titulo").innerText = botao.dataset.titulo;
+    document.getElementById("modal-imagem").src = botao.dataset.imagem;
+    document.getElementById("modal-descricao").innerText = botao.dataset.descricao;
+    document.getElementById("modal-preco").innerText = botao.dataset.preco;
+    modal.style.display = "block";
+}
+
+var botoesVerMais = document.querySelectorAll(".ver-mais");
+
+botoesVerMais.forEach(function(botao) {
+    botao.addEventListener("click", function() {
+        abrirModal(botao);
+    });
+});
+
+function fecharModal() {
+    var modal = document.getElementById("modalProduto");
+    modal.style.display = "none";
+}
